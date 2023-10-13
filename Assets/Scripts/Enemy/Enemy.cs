c246e3ed4cb8663cabe5d1953823fabb383a8cb6
Berk <berkkarakas98@hotmail.com>
2023-10-13 21:00:19 +0300

    Implement base class for enemies

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,9 +2,68 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
+    [Header("Enemy Stats")]
     [SerializeField] protected int health;
     [SerializeField] protected int speed;
     [SerializeField] protected int gems;
 
-    public abstract void Update();
+    [Header("Enemy Waypoint")]
+    [SerializeField] protected Transform waypointA;
+    [SerializeField] protected Transform waypointB;
+    protected Vector3 currentTarget;
+
+    [Header("Enemy Animation")]
+    protected bool idleInitialState = true;
+
+    [Header("Enemy References")]
+    protected Animator animator;
+
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        Flip();
+        Movement();
+    }
+
+    public virtual void Movement()
+    {
+        if (transform.position == waypointA.position)
+        {
+            currentTarget = waypointB.position;
+            if (!idleInitialState)
+                animator.SetTrigger("Idle");
+        }
+        else if (transform.position == waypointB.position)
+        {
+            currentTarget = waypointA.position;
+            idleInitialState = false;
+            animator.SetTrigger("Idle");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+    public virtual void Flip()
+    {
+        if (currentTarget == waypointA.position)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
 }
--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -3,57 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MossGiant : Enemy
-{
-    [SerializeField] private Transform _waypointA, _waypointB;
-    private bool _idleInitialState;
-    private Vector3 _currentTarget;
-    private Animator _animator;
-
-    private void Start()
+{ 
+    // Initilize
+    public override void Init()
     {
-        _animator = GetComponentInChildren<Animator>();
+        base.Init();
     }
-
-    public override void Update()
-    {
-        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Moss_Giant_Idle"))
-        {
-            _idleInitialState = true;
-            return;
-        }
-        Flip();
-        Movement();
-    }
-
-    private void Movement()
-    {
-        if (transform.position == _waypointA.position)
-        {
-            _currentTarget = _waypointB.position;
-            if (!_idleInitialState)
-            {            
-                _animator.SetTrigger("Idle");
-                _idleInitialState = false;
-            }
-        }
-        else if (transform.position == _waypointB.position)
-        {
-            _currentTarget = _waypointA.position;
-            _animator.SetTrigger("Idle");
-        }
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-    }
-
-    private void Flip()
-    {
-        if (_currentTarget == _waypointA.position)
-        {
-            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-        }
-        else
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        }
-    }
-
 }
+
