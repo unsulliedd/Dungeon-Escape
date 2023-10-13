5037f8db3110fb4c4a1288fd89f4a50dc05d7e14
Berk <berkkarakas98@hotmail.com>
2023-10-13 21:38:41 +0300

    Add spider and skeleton enemy

---
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : Enemy
+{
+    // Initilize
+    public override void Init()
+    {
+        base.Init();
+    }
+}
---
+++ Assets/Scripts/Enemy/Spider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    // Initilize
+    public override void Init()
+    {
+        base.Init();
+    }
+}
