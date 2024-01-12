155d747e9a9a1ab2dd32ccd776cb435cdae166ff
Berk <berkkarakas98@hotmail.com>
2024-01-12 20:20:05 +0300

    Rework enemy script
    - Waypoint comparisons are now performed using square distance comparisons between positions (Vector3.SqrMagnitude).
    - Added a new method, CalculateDistanceToPlayer, to compute the distance and horizontal direction from the enemy to the player.
    - Introduced a new method, IsBetweenWaypoints, to determine if the player is positioned between the waypoints using the dot product of normalized vectors.
    - Refactored the code by introducing variables for some numerical values and improved the expressiveness of certain names for better readability and maintainability.
    - Add comments

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,83 +1,101 @@
 using UnityEngine;
 
+// This abstract class represents the base functionality for enemy characters in the game.
 public abstract class Enemy : MonoBehaviour
 {
     [Header("Enemy Stats")]
-    [SerializeField] protected int health;
-    [SerializeField] protected int speed;
-    [SerializeField] protected int gems;
-    protected bool isDead = false;
+    [SerializeField] protected int health;                      // The health points of the enemy.
+    [SerializeField] protected int speed;                       // The movement speed of the enemy.
+    [SerializeField] protected int gems;                        // The amount of gems rewarded upon defeating the enemy.
+    protected bool isDead = false;                              // Flag indicating if the enemy is dead.
 
     [Header("Enemy Waypoint")]
-    [SerializeField] protected Transform waypoint1;
-    [SerializeField] protected Transform waypoint2;
-    protected Vector3 currentTarget;
-    protected float distance;
-    protected float gapBetweenPlayer = 1.5f;
+    [SerializeField] protected Transform waypointStart;         // The starting waypoint for enemy movement.
+    [SerializeField] protected Transform waypointEnd;           // The ending waypoint for enemy movement.
+    [SerializeField] protected bool isLookLeft;                 // Flag indicating initial facing direction.
+    protected Vector3 waypoint1;                                // Position of the first waypoint.
+    protected Vector3 waypoint2;                                // Position of the second waypoint.
+    protected Vector3 currentTarget;                            // The current target waypoint for movement.
+    [SerializeField] protected float minAttackDistancce = 3f;   // The distance at which the enemy can attack the player.
+    [SerializeField] protected float minDistancetoPlayer = 6f;  // The minimum distance between the enemy and the player.
+    private float distance;                                     // Distance between the enemy and the player.
+    private float dirDistance;                                  // Directional horizontal distance between the enemy and the player.
+    protected float gapBetweenPlayer = 1.5f;                    // Gap between the enemy and the player.
 
     [Header("Enemy Animation")]
-    protected bool idleInitialState = true;
+    protected bool idleInitialState = true;                     // Initial state flag for the idle animation.
 
     [Header("Enemy References")]
-    protected Animator animator;
-    protected Player player;
-    protected bool playerAlive;
-    [SerializeField] protected GameObject _diamondPrefab;
+    protected Animator animator;                                // Reference to the enemy's animator component.
+    protected Player player;                                    // Reference to the player character.
+    protected bool playerAlive;                                 // Flag indicating if the player is alive.
+    [SerializeField] protected GameObject _diamondPrefab;       // Reference to the diamond prefab for drops.
 
-    private void Awake()
+    void Awake()
     {
+        // Retrieve the animator component and player reference.
         animator = GetComponentInChildren<Animator>();
         if (animator == null)
             Debug.LogError("Animator is NULL");
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        if (player == null)
+        if (!GameObject.FindGameObjectWithTag("Player").TryGetComponent(out player))
             Debug.LogError("Player is NULL");
-    }
 
-    public virtual void Init()
-    {
-        currentTarget = waypoint1.position;
-    }
-
-    private void Start()
-    {
+        // Initialize the enemy.
         Init();
     }
 
-    public virtual void Update()
+    // Initializes the enemy with waypoint positions and initial states.
+    public virtual void Init()
     {
+        waypoint1 = waypointStart.position;
+        waypoint2 = waypointEnd.position;
+
+        waypointStart.gameObject.SetActive(false);
+        waypointEnd.gameObject.SetActive(false);
+
+        currentTarget = waypoint1;
+
+        waypoint1.y = transform.position.y;
+        waypoint2.y = transform.position.y;
+    }
+
+    void Update()
+    {
+        // Check if the player is alive.
         playerAlive = player.IsPlayerAlive();
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && animator.GetBool("InCombat") == false)
+
+        // Update player status and handle movement if not in idle or combat animation.
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && !animator.GetBool("InCombat"))
             return;
-        if (isDead == false)
+
+        // Calculate the distance between the enemy and the player.
+        distance = CalculateDistanceToPlayer().distance;
+        dirDistance = CalculateDistanceToPlayer().dirDistance;
+
+        if (!isDead)
         {
-            Flip();
-            Movement();
+            Flip(dirDistance, distance);
+            Movement(dirDistance, distance);
         }
     }
 
-    public virtual void Movement()
-    {            
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        float distanceX = transform.position.x - player.transform.position.x;
-
-        if (distanceX < 0)
-            gapBetweenPlayer *= -1;
-
-        Vector2 targetPlayerDestination = new(player.transform.position.x + gapBetweenPlayer, transform.position.y);
-
-        if (distance > 6f || !playerAlive)
+    // Handles the enemy movement logic.
+    private void Movement(float dirDistance,float distance)
+    {
+        // If the player is not within a certain distance, move between waypoints.
+        if (distance > 6f || !playerAlive || !IsBetweenWaypoints())
         {
-            if (transform.position == waypoint1.position)
+            // Move towards the target waypoint based on the current position.
+            if (Vector3.SqrMagnitude(transform.position - waypoint1) < 0.05)
             {
                 if (!idleInitialState)
                     animator.SetTrigger("Idle");
-                currentTarget = waypoint2.position;
+                currentTarget = waypoint2;
             }
-            else if (transform.position == waypoint2.position)
+            else if (Vector3.SqrMagnitude(transform.position - waypoint2) < 0.05)
             {
                 animator.SetTrigger("Idle");
-                currentTarget = waypoint1.position;
+                currentTarget = waypoint1;
                 idleInitialState = false;
             }
 
@@ -85,41 +103,75 @@ public abstract class Enemy : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
         }
 
-        if (distance < 6f && playerAlive
-            // if player is in between waypoints
-            && transform.position.x >= waypoint1.position.x 
-            && transform.position.x <= waypoint2.position.x) 
+        // If the player is within a certain distance, move towards the player.
+        if (distance < minDistancetoPlayer && playerAlive && IsBetweenWaypoints())
         {
-            if (distance < 3f)
+            // Adjust the gap between the player and the enemy based on their positions.
+            if (dirDistance > 0)
+                gapBetweenPlayer *= -1;
+
+            // If the player is within a certain distance, update the enemy's position to be close to the player with a specified gap.
+            Vector2 targetPlayerDestination = new(player.transform.position.x + gapBetweenPlayer, transform.position.y);
+
+            if (distance < minAttackDistancce)
             {
                 animator.SetBool("InCombat", true);
                 animator.SetTrigger("Idle");
             }
             else
                 animator.SetBool("InCombat", false);
-            transform.position = Vector2.MoveTowards(transform.position, targetPlayerDestination, speed * Time.deltaTime);
-        }
 
-        Debug.DrawLine(transform.position, currentTarget, Color.green);
+            // Move towards the player within the specified distance.
+            if (distance > minAttackDistancce/2)
+                transform.position = Vector2.MoveTowards(transform.position, targetPlayerDestination, speed * Time.deltaTime);
+        }
     }
 
-    public virtual void Flip()
+    // Flips the enemy's facing direction based on the player's position.
+    private void Flip(float dirDistance, float distance)
     {
-        float direction = player.transform.position.x - transform.position.x;
-
-        if (distance > 6f || !playerAlive)
+        if (distance > minDistancetoPlayer || !playerAlive || !IsBetweenWaypoints())
         {
-            if (currentTarget == waypoint1.position)
-                transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            // Flip the enemy based on the facing direction and initial look direction.
+            if (isLookLeft)
+            {
+                if (currentTarget == waypoint1)
+                    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                else
+                    transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+            }
             else
-                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            {
+                if (currentTarget == waypoint1)
+                    transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                else
+                    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
         }
-        else if (distance < 6f && playerAlive)
+        else if (distance < minDistancetoPlayer && playerAlive && IsBetweenWaypoints())
         {
-            if (direction > 0)
+            // Flip the enemy based on the direction to the player.
+            if (dirDistance > 0)
                 transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            else if (direction < 0)
+            else if (dirDistance < 0)
                 transform.rotation = Quaternion.Euler(0f, 180f, 0f);
         }
+    }
+
+    // Checks if the player is between the waypoints of the enemy.
+    private bool IsBetweenWaypoints()
+    {
+        // Check if the dot product of the normalized vectors is less than 0,
+        // indicating that the player is between waypoint1 and waypoint2 in both cases.
+        return Vector3.Dot((waypoint2 - waypoint1).normalized, (player.transform.position - waypoint2).normalized) < 0f
+            && Vector3.Dot((waypoint1 - waypoint2).normalized, (player.transform.position - waypoint1).normalized) < 0f;
+    }
+
+    // Calculates the distance between the enemy and the player.
+    private (float distance, float dirDistance) CalculateDistanceToPlayer()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        dirDistance = player.transform.position.x - transform.position.x;
+        return (distance, dirDistance);
     }
 }
